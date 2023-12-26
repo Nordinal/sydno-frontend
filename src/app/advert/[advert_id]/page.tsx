bd990228721d7161cb417a78d77dd1283df06f03
Gleb Marenin <glebmarenin20@gmail.com>
2023-12-26 16:35:58 +0300

    feat: added basic advert layout

--- src/app/advert/[advert_id]/page.tsx
+++ src/app/advert/[advert_id]/page.tsx
@@ -1,95 +1,196 @@
-'use client';
-import React, { useEffect, useState } from "react";
-import { Col, Row, Typography } from "antd";
-import { IAdvertListItem, useAdvert } from "@/entities/advert";
+"use client";
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { Button, Col, Image, Row, Spin, Typography } from "antd";
+import { StarFilled } from "@ant-design/icons";
+import { useAdvert } from "@/entities/advert";
 import { useShallow } from "zustand/react/shallow";
 import SmallImageSlider from "@/shared/ui/SmallImageSlider";
 import Price from "@/shared/ui/Price";
-import { Carousel } from 'antd';
-
+import { Carousel } from "antd";
+import "./styles.css";
+import { DetailsPair } from "./DetailsPair";
+import { ConvertData } from "./DataConverter";
+import { IAdvertListItem } from "@/entities/advert/types/main";
 interface IAdvertPageProps {
-    params?: {
-        advert_id?: string;
-    }
+  params?: {
+    advert_id?: string;
+  };
 }
 
-const PRICE_LOCALE = 'ru';
+const PRICE_LOCALE = "ru";
 
 const NUMBER_FORMAT_OPTIONS = {
-    style: 'currency',
-    currency: 'RUB'
-}
-
-const contentStyle: React.CSSProperties = {
-    margin: 0,
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
+  style: "currency",
+  currency: "RUB",
 };
 
 const AdvertPage: React.FC<IAdvertPageProps> = ({ params }) => {
-    const { getAdvert } = useAdvert(useShallow(state => ({ getAdvert: state.getAdvert })));
-    const [advertData, setAdvertData] = useState<IAdvertListItem | false>();
+  const { getAdvert } = useAdvert(
+    useShallow((state) => ({ getAdvert: state.getAdvert }))
+  );
+  const [advertData, setAdvertData] = useState<IAdvertListItem | false>();
+  const [showAllCharacteristics, setShowAllCharacteristics] = useState(false);
+  const [showNumber, setShowNumber] = useState<boolean>(false);
 
-    useEffect(() => {
-        getAdvert(Number(params?.advert_id))
-            .then((data) => {
-                if (data === false) {
+  const showNumberBtnHandler = (e: SyntheticEvent) => {
+    e.stopPropagation();
+    setShowNumber(true);
+  };
 
-                } else {
-                    setAdvertData(data);
-                }
-            });
-    }, []);
+  useEffect(() => {
+    getAdvert(Number(params?.advert_id)).then((data) => {
+      if (data === false) {
+      } else {
+        setAdvertData(data);
+      }
+    });
+  }, []);
 
+  //Готовим данные для отрисовки
+  const ConvertedAdvertData = advertData && ConvertData(advertData);
+
+  if (!advertData) {
     return (
-        <div className="pt-6">
-            <Typography.Title level={1}>
-                Скоро тут будет полноценная страница 
-            </Typography.Title>
-            <Row gutter={16}>
-                <Col span={12}>
-                    {/* <Carousel>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                        <div>
-                            <h3 style={contentStyle}>1</h3>
-                        </div>
-                    </Carousel> */}
-                    <SmallImageSlider
-                        items={(advertData as any)?.images || []}
-                        maxItems={5}
-                        showLabels={true}
-                        imageClass='rounded-xl'
-                    />
-                </Col>
-                <Col span={12}>
-                    <Typography.Title level={2}>
-                        <Price
-                            locale={PRICE_LOCALE}
-                            options={NUMBER_FORMAT_OPTIONS}
-                            price={(advertData as any)?.price || 0}
-                        />
-                    </Typography.Title>
-                </Col>
-            </Row>
-        </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <Spin size="large" />
+      </div>
     );
-}
+  }
+  return (
+    <div className="pt-6">
+      <Row gutter={14}>
+        <Col span={12}>
+          <Typography.Title level={1}>
+            <StarFilled className="favorite-star" />
+            {advertData.header}
+          </Typography.Title>
+        </Col>
+
+        <Col span={9} className="price-phone">
+          <Typography.Title level={2} className="price">
+            <Price
+              locale={PRICE_LOCALE}
+              options={NUMBER_FORMAT_OPTIONS}
+              price={advertData.price || 0}
+            />
+          </Typography.Title>
+          <Typography.Paragraph>
+            {showNumber ? (
+              <Button
+                type="primary"
+                href={`tel:${advertData.phone_number}`}
+              >{`Позвонить +${advertData.phone_number}`}</Button>
+            ) : (
+              <Button onClick={showNumberBtnHandler}>Показать телефон</Button>
+            )}
+          </Typography.Paragraph>
+        </Col>
+      </Row>
+
+      <Row gutter={24}>
+        <Col span={12}>
+          <Carousel className="custom-carousel">
+            {advertData &&
+              (advertData?.images || []).map((image, index) => (
+                <div key={index}>
+                  <Image
+                    src={image}
+                    alt={`Image ${index}`}
+                    className="full-width-image"
+                  />
+                </div>
+              ))}
+          </Carousel>
+          <SmallImageSlider
+            items={advertData.images || []}
+            maxItems={5}
+            showLabels={true}
+            // imageClass="rounded-xl"
+          />
+          <Typography.Title level={4} className="description">
+            Описание
+          </Typography.Title>
+          <Typography.Paragraph>{advertData.description}</Typography.Paragraph>
+        </Col>
+
+        <Col span={12}>
+          <div>
+            <Row gutter={24}>
+              <Col span={16}>
+                <div>
+                  {ConvertedAdvertData &&
+                    ConvertedAdvertData.mainInfo.map(
+                      (field, index) =>
+                        field.value && (
+                          <DetailsPair
+                            key={index}
+                            title={field.title}
+                            value={field.value}
+                          />
+                        )
+                    )}
+                  <div>
+                    {!showAllCharacteristics && ConvertedAdvertData && (
+                      <Button
+                        className="all-button"
+                        onClick={() => setShowAllCharacteristics(true)}
+                      >
+                        Показать все характеристики
+                      </Button>
+                    )}
+
+                    {showAllCharacteristics && ConvertedAdvertData && (
+                      <>
+                        <Typography.Title level={4} className="info-title">
+                          Юридическая информация
+                        </Typography.Title>
+                        {ConvertedAdvertData.legalInfo.map(
+                          (field, index) =>
+                            field.value && (
+                              <DetailsPair
+                                key={index}
+                                title={field.title}
+                                value={field.value}
+                              />
+                            )
+                        )}
+                        <Typography.Title level={4} className="info-title">
+                          Техническая информация
+                        </Typography.Title>
+                        {ConvertedAdvertData.technicalInfo.map(
+                          (field, index) =>
+                            field !== null && (
+                              <DetailsPair
+                                key={index}
+                                title={field.title}
+                                value={field.value}
+                              />
+                            )
+                        )}
+                        <Button
+                          className="all-button"
+                          onClick={() => setShowAllCharacteristics(false)}
+                        >
+                          Скрыть
+                        </Button>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default AdvertPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,29 @@
-
-import { Metadata } from 'next';
-import { StyledComponentsRegistry } from '@/shared/ui/AntdRegistry';
-import { instanceApi } from '@/shared/configs/instanceAxios';
-import { AxiosError, AxiosPromise } from 'axios';
-import { MainLayoutServer } from '@/layout/MainLayout/layout-server';
-import { cookies } from 'next/headers';
+import { Metadata } from "next";
+import { StyledComponentsRegistry } from "@/shared/ui/AntdRegistry";
+import { instanceApi } from "@/shared/configs/instanceAxios";
+import { AxiosError, AxiosPromise } from "axios";
+import { MainLayoutServer } from "@/layout/MainLayout/layout-server";
+import { cookies } from "next/headers";
 
 export const metadata: Metadata = {
-  title: 'Главная страница',
-  description: '123',
+  title: "Главная страница",
+  description: "123",
   icons: {
-    icon: ['/favicon.ico?v=4'],
-    apple: ['/apple-touch-icon.png?v=4'],
-    shortcut: ['/apple-touch-icon.png'],
-  }
+    icon: ["/favicon.ico?v=4"],
+    apple: ["/apple-touch-icon.png?v=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
 };
 
-export default async function RootLayout(props: {
-  children: React.ReactNode;
-}) {
-
+export default async function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html>
       <head />
       <body>
         <StyledComponentsRegistry>
-            <MainLayoutServer>
-              {props.children}
-            </MainLayoutServer>
+          <MainLayoutServer>{props.children}</MainLayoutServer>
         </StyledComponentsRegistry>
+        <div id="portal" />
       </body>
     </html>
   );
